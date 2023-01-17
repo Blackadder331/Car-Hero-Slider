22c743732fd6147a6f9ba92558909367baf5ecfd
Reynolds Sharp <sharpdesignco@Reynoldss-iPro.attlocal.net>
2023-01-16 18:59:24 -0600

    working sliders

---
+++ hero-scripts.js
@@ -0,0 +1,28 @@
+
+const slides = document.querySelectorAll(".slide");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+let index = 0;
+
+// Show initial slide
+slides[index].classList.add("active");
+
+// Function to go to previous slide
+prev.addEventListener("click", () => {
+  slides[index].classList.remove("active");
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  slides[index].classList.add("active");
+});
+
+// Function to go to next slide
+next.addEventListener("click", () => {
+  slides[index].classList.remove("active");
+  index++;
+  if (index === slides.length) {
+    index = 0;
+  }
+  slides[index].classList.add("active");
+});
